bae8fd9f29526640413fe354193a766b09c34039
Your Namezl28 <you@example.com>
2020-07-24 17:17:45 -0400

    added model architecture, 07/24/2020

---
+++ config.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020-07-24 16:06
+# @Author  : Zongchao Liu
+# @FileName: config.py
+# @Software: PyCharm
---
+++ main.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020-07-24 16:06
+# @Author  : Zongchao Liu
+# @FileName: main.py
+# @Software: PyCharm
